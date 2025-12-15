32d5ed13031592840fe0204f7544acd3d71cedd0
Superhans79 <43516655+Superhans79@users.noreply.github.com>
2025-12-15 21:27:25 +0000

    Add user input and variables

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Nice to meet you, " + name + "!");
+
+        scanner.close();
     }
 }
